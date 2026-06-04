19b75608c23330e7fe53cce50e28f16209eadd93
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-06-04 12:05:40 +0200

    First commit

--- back-end-project/routes/UserRoute.js
+++ back-end-project/routes/UserRoute.js
@@ -57,7 +57,29 @@ router.post('/login', async (req, res) => {
             if (!isPasswordTrue) {
                 return res.status(401).json({ message: 'Incorrect password' });
             } 
+
+
+            req.session.user = {
+                user_id = IsUsernameExist[0].user_id,
+                username = IsUsernameExist[0].username
+            };
+
+            return res.status(200).json({ message: 'Logged in successfully' });
     } catch (err) {
         console.error(err);
     }
 });
+
+router.post('/logout', async (req, res) => {
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.status(500).json({ message: 'Failed to log out'});
+            }
+
+            return res.status(200).json({ message: 'Logged out successfully' });
+        });
+    } catch (err) {
+        console.error(err);
+    }
+})
